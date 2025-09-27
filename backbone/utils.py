2c87a376219ed159b3705df380d4a75af7c528c7
Bunga Razvan <razvanbunga@gmail.com>
2025-09-27 21:07:31 +0100

    add docker

--- backbone/utils.py
+++
@@ -1,9 +0,0 @@
-def db_object(env, db_name, abbr):
-    return {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': f'{db_name}_{env("ENV")}',
-        'USER': env(f'{abbr}_USER'),
-        'PASSWORD': env(f'{abbr}_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '3306',
-    }
