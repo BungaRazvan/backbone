e6c4dc3efd79b24275751130fa8de0e35ee0841a
Bunga Razvan <razvanbunga@gmail.com>
2025-10-04 16:44:22 +0100

    docker deployment changes

--- backbone/settings.py
+++ backbone/settings.py
@@ -156,3 +156,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+CSRF_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", default=False)
+SESSION_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", default=False)
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
--- run.sh
+++ run.sh
@@ -5,14 +5,20 @@ docker rm backbone -f
 
 docker load -i backbone.tar
 
-docker run -d   --name backbone   -p 8000:8000   --env-file /home/pi/projects/backbone/.env.prod   --security-opt seccomp=unconfined   backbone:latest
+docker run -d \
+    --name backbone \
+    -p 8000:8000 \
+    --env-file /home/pi/projects/backbone/.env.prod \
+    -v /home/pi/projects/backbone/databases:/app/database \
+    -v /home/pi/projects/backbone/staticfiles:/app/staticfiles \
+    --security-opt seccomp=unconfined   backbone:latest
 
 sleep 5
 
-docker run exec -it backbone ./manage.py collectstatic --noinput
+docker exec -it backbone ./manage.py collectstatic --noinput
 
-docker run exec -it backbone ./manage.py migrate
+docker exec -it backbone ./manage.py migrate
 
-docker run exec -it backbone  ./manage.py migrate --database=discord_db
+docker exec -it backbone  ./manage.py migrate --database=discord_db
 
-docker run exec -it backbone  ./manage.py migrate --database=extension_db
+docker exec -it backbone  ./manage.py migrate --database=extension_db
